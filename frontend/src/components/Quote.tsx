de6162ba5aaf4a7f020babe50c71f573b415e958
Aaditya Malani <aadityamalanii15@gmail.com>
2024-06-27 15:51:29 +0530

    forntend started

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,11 @@
+export const Quote = () => {
+  return (
+    <div className=" bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+        <div className=" max-w-md text-center text-2xl font-bold">
+           Quote
+        </div>
+        </div>
+    </div>
+  );
+};
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,10 @@
+import { Quote } from "../components/Quote";
+
+
+export const Signup = () => {
+  return <div>
+    <Quote/>
+    Signup</div>;
+};
+
+
